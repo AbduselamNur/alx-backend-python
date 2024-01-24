2cbbe67e0b3c3864c39f4b17ff8d28dd52f3a6cd
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-01-24 07:50:05 +0300

    Fix type hint in sum_list function

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+from typing import List
 """
 #!/usr/bin/env python3
 """
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     sum_list: takes a list input_list of floats as argument
     and returns their sum as a float."""
