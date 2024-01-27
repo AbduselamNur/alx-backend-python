25159e8449a72520eb8155ca9a1d48e4c9cff44b
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-01-27 08:01:42 +0300

    Fix type hint in async_comprehension function

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
 async_generator = __import__('0-async_generator').async_generator
+from typing import List
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> List[float]:
     """Async Comprehensions"""
     return [i async for i in async_generator()]
