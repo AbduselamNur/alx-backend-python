0c76e2673abed5eada8add96f37e9729e5e81d8d
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2024-01-27 07:59:23 +0300

    Update return type annotation in async_comprehension function

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,6 +3,6 @@
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> list[float]:
     """Async Comprehensions"""
     return [i async for i in async_generator()]
